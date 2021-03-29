daf07411db46c692ea70ac858ac69f1263fefced
PositivePeriod <positiveperiod.dev@gmail.com>
2021-03-30 01:01:04 +0900

    feat: AreaGlyph

---
+++ public/AreaGlyph/app.js
@@ -0,0 +1,154 @@
+import * as create from './createDomElement.js'
+
+const CircleRadius = 0.5;
+
+async function loadFontFromUrl(url) {
+    var font = await opentype.load(url);
+    return font;
+};
+
+class App {
+    constructor(font) {
+        this.font = font;
+
+        this.bgSVG = create.createSVGElement(0, 0);
+        this.bgSVG.setAttribute('id', 'bgSVG');
+        document.body.appendChild(this.bgSVG);
+
+        this.domGroup = create.createGroupElement();
+        this.domGroup.setAttribute('id', 'domGroup')
+        this.bgSVG.appendChild(this.domGroup);
+
+        document.getElementById('inputForm').addEventListener('input', this.updateGlyph.bind(this), false)
+        window.addEventListener("resize", this.replaceGlyph.bind(this), false);
+        document.getElementById('inputButton').addEventListener('click', this.calculate.bind(this), false)
+
+        this.updateGlyph();
+        this.replaceGlyph();
+    }
+
+    updateGlyph() {
+        this.getInput();
+        var pointData = this.createDOMElements();
+        this.directPoints = pointData['direct'];
+        this.indirectPoints = pointData['indirect'];
+
+        var glyphData = this.replaceGlyph();
+        this.glpyhWidth = glyphData['width'];
+        this.glyphHeight = glyphData['height'];
+    }
+
+    calculate() {
+        var glyphArea = Math.abs(polygonArea(this.directPoints));
+        var boxArea = this.glpyhWidth * this.glyphHeight;
+        var ratio = glyphArea / boxArea * 100;
+        // console.log(this.str, glyphArea, boxArea, glyphArea/boxArea);
+        console.log(this.str, ratio.toFixed(2));
+    }
+
+    getInput() {
+        this.str = document.getElementById('inputStr').value;
+        var fontSize = parseInt(document.getElementById('inputSize').value);
+        if (isNaN(fontSize)) {
+            return false;
+        } else {
+            this.fontSize = fontSize;
+        }
+
+        // Find the real size of glyph
+        var tempStrPath = this.font.getPath(this.str, 0, 0, this.fontSize);
+        var strBox = tempStrPath.getBoundingBox();
+
+        // Apply the discovered size
+        this.strPath = this.font.getPath(this.str, -strBox.x1, -strBox.y1, this.fontSize);
+        this.strBox = this.strPath.getBoundingBox();
+
+        // glyph = font.charToGlyph(str)
+        // path = glyph.getPath()
+        // path = glyph.getBoundingBox()
+    }
+
+    createDOMElements() {
+        while (this.domGroup.hasChildNodes()) {
+            var child = this.domGroup.lastElementChild;
+            this.domGroup.removeChild(child);
+        }
+
+        this.data = create.getDataFromCommands(this.strPath.commands);
+        this.dGroup = this.data[0];
+        this.directPoints = this.data[1];
+        this.indirectPoints = this.data[2];
+        this.additionalLine = this.data[3];
+
+        this.dGroup.forEach(d => {
+            this.domGroup.appendChild(create.createPathElement(d, this.stageWidth, this, this.stageHeight));
+        })
+
+        var domDirect = create.createGroupElement();
+        domDirect.setAttribute('id', 'directPoints')
+        this.directPoints.forEach(point => {
+            var circle = create.createCircleElement(point.x, point.y, CircleRadius);
+            circle.classList.add('direct');
+            domDirect.appendChild(circle);
+        })
+        this.domGroup.appendChild(domDirect);
+
+        var domIndirect = create.createGroupElement();
+        domIndirect.setAttribute('id', 'indirectPoints');
+        this.indirectPoints.forEach(point => {
+            var circle = create.createCircleElement(point.x, point.y, CircleRadius);
+            circle.classList.add('indirect');
+            domIndirect.appendChild(circle);
+        })
+        this.domGroup.appendChild(domIndirect);
+        return { direct: this.directPoints, indirect: this.indirectPoints, }
+    }
+
+    replaceGlyph() {
+        var existEdge = this.domGroup.querySelector(".edge");
+        if (existEdge) {
+            existEdge.remove();
+        }
+        var glpyhWidth = this.strBox.x2 - this.strBox.x1;
+        var glyphHeight = this.strBox.y2 - this.strBox.y1;
+        var windowWidth = window.innerWidth;
+        var windowHeight = window.innerHeight;
+
+        // console.log('glyph', glpyhWidth, glyphHeight);
+        // console.log('window', windowWidth, windowHeight);
+
+        this.bgSVG.setAttribute('x', 0);
+        this.bgSVG.setAttribute('y', 0);
+        this.bgSVG.setAttribute('width', windowWidth);
+        this.bgSVG.setAttribute('height', windowHeight);
+
+        var posX = (windowWidth - glpyhWidth) / 2;
+        var poxY = (windowHeight - glyphHeight) / 2;
+        this.domGroup.setAttribute("transform", `translate(${posX}, ${poxY})`);
+
+        var edge = create.createRectElement(0, 0, glpyhWidth, glyphHeight);
+        edge.classList.add('edge')
+        this.domGroup.appendChild(edge);
+        return { width: glpyhWidth, height: glyphHeight }
+    }
+
+}
+
+function polygonArea(pts) {
+    var area = 0;
+    for (let i = 0; i < pts.length - 1; i++) {
+        area += (pts[i + 1].x + pts[i].x) * (pts[i + 1].y - pts[i].y);
+    }
+    area += (pts[0].x + pts[pts.length - 1].x) * (pts[0].y - pts[pts.length - 1].y);
+    return area / 2;
+}
+
+window.onload = () => {
+    var fontUrl = 'http://cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebBold.woff';
+
+    var fontName = 'TmonMonsori.otf.otf';
+    var fontUrl = `../data/${fontName}`;
+    loadFontFromUrl(fontUrl).then((font) => {
+        new App(font);
+    })
+};
---
+++ public/AreaGlyph/createDomElement.js
@@ -0,0 +1,125 @@
+const NAMESPACE = "http://www.w3.org/2000/svg";
+
+function setSizeElement(element, width, height) {
+    element.setAttribute("width", width);
+    element.setAttribute("height", height);
+}
+
+function setStyleElement(element, stroke, fill) {
+    element.setAttribute("stroke", stroke);
+    element.setAttribute("fill", fill);
+}
+
+export function createSVGElement(width, height, stroke = "#ffffff", fill = "transparent") {
+    var element = document.createElementNS(NAMESPACE, "svg");
+    setSizeElement(element, width, height);
+    setStyleElement(element, stroke, fill);
+    return element;
+}
+
+export function createPathElement(d, width, height, stroke = "#ffffff", fill = "transparent") {
+    var element = document.createElementNS(NAMESPACE, "path");
+    element.setAttribute("d", d)
+    setSizeElement(element, width, height);
+    setStyleElement(element, stroke, fill);
+    return element;
+}
+
+export function createCircleElement(x, y, r = 3, stroke = "#ffffff", fill = "transparent") {
+    var element = document.createElementNS(NAMESPACE, "circle");
+    element.setAttribute("cx", x);
+    element.setAttribute("cy", y);
+    element.setAttribute("r", r);
+    setStyleElement(element, stroke, fill);
+    return element;
+}
+
+export function createRectElement(x, y, width, height, stroke = "#ffffff", fill = "transparent") {
+    var element = document.createElementNS(NAMESPACE, "rect");
+    element.setAttribute("x", x);
+    element.setAttribute("y", y);
+    setSizeElement(element, width, height);
+    setStyleElement(element, stroke, fill);
+    return element;
+}
+
+export function createGroupElement(stroke = "#ffffff", fill = "transparent") {
+    var element = document.createElementNS(NAMESPACE, "g");
+    setStyleElement(element, stroke, fill);
+    return element;
+}
+
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+export function getDataFromCommands(commands) {
+    var d = "";
+    var dGroup = [];
+    var pos, newPos;
+    var directPoints = [];
+    var indirectPoints = [];
+    var additionalLine = [];
+    var firstControlPoint, secondControlPoint;
+    commands.forEach((command) => {
+        switch (command.type) { 
+            case "M":
+            case "L":
+                d += `${command.type} ${command.x} ${command.y}`;
+                pos = new Point(command.x, command.y);
+                directPoints.push(pos);
+                break;
+            case "C":
+                d += `${command.type} ${command.x1} ${command.y1} ${command.x2} ${command.y2} ${command.x} ${command.y}`;
+                firstControlPoint = new Point(command.x1, command.y1);
+                secondControlPoint = new Point(command.x2, command.y2);
+                newPos = new Point(command.x, command.y);
+                indirectPoints.push(firstControlPoint);
+                indirectPoints.push(secondControlPoint);
+                directPoints.push(newPos);
+                additionalLine.push([pos, firstControlPoint]);
+                additionalLine.push([secondControlPoint, newPos]);
+                pos = newPos;
+                break;
+            case "Z":
+            case "z":
+                d += `${command.type}`
+                dGroup.push(d);
+                d = ""
+                break;
+            case "Q":
+                d += `${command.type} ${command.x1} ${command.y1} ${command.x} ${command.y}`;
+                firstControlPoint = new Point(command.x1, command.y1);
+                newPos = new Point(command.x, command.y);
+                indirectPoints.push(firstControlPoint);
+                directPoints.push(newPos);
+                additionalLine.push([pos, firstControlPoint]);
+                additionalLine.push([firstControlPoint, newPos]);
+                pos = newPos;
+                break;
+            case "m":
+            case "l":
+            case "H":
+            case "h":
+            case "V":
+            case "v":
+            case "c":
+            case "S":
+            case "q":
+            case "T":
+            case "t":
+            case "A":
+            case "a":
+                alert(`Warning: Implement command type ${command.type}`);
+                break;
+            default:
+                alert(`Error: Impossible command type ${command.type}`);
+                break;
+        }
+    });
+    return [dGroup, directPoints, indirectPoints, additionalLine]
+}
